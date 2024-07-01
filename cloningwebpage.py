d30311aa56f92797b3612eaa79b1baacd4d5d745
Purohit-Namrata <namratapurohit78@gmail.com>
2024-07-01 18:07:25 +0530

    Add files via upload

---
+++ cloningwebpage.py
@@ -0,0 +1,11 @@
+from pywebcopy import save_web_page
+
+kwargs = {'project_name': 'site folder'}
+
+save_web_page(
+
+	
+	url='https://www.w3schools.com/python/',
+	
+	project_folder='C:/Users/BLAUPLUG/Documents/Python_programs/cloning webpage',**kwargs
+)
